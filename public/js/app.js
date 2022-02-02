93d41d79e25e20a20427f60ba9fcd46e728ecd39
Deepashri Dali <Deepashri.Dali@softwire.com>
2022-02-02 22:48:39 +0000

    Added mobile menu with JS

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Add your JavaScript here.
+    
+    const colourChangeBtn = document.getElementById("my-button");
+    const postCol = document.getElementsByClassName('post');
+    const mobMenu = document.getElementById("mobile-menu");
+    //Change background
+
+    colourChangeBtn.addEventListener("click", function(){
+        for (const post of postCol) {
+        post.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`
+        }
+    });
+    // Hamburger menu
+    const crossBtn = document.getElementById("cross-btn");
+    crossBtn.addEventListener("click", function(){
+    mobMenu.classList.toggle("mobile-menu-hidden");
+    })
+    
+    const hamBtn = document.getElementById("ham-btn");
+    hamBtn.addEventListener("click", function(){
+    mobMenu.classList.toggle("mobile-menu-open");
+    });
+
+ 
+
 });
